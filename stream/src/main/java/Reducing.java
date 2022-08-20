5012ba658eebdad7bfc8d39a81c808529b5d7e6c
emreceva <86777959+emreceva@users.noreply.github.com>
2022-08-20 16:48:17 +0300

    reducing

---
+++ stream/src/main/java/Reducing.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Reducing {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(3,5,6,8,9,4);
+        int result = numbers.stream().reduce(0,(a,b) ->(a+b));
+        System.out.println("result = " + result);
+
+        //No initial value
+        Optional<Integer> result2 = numbers.stream().reduce((a, b) -> (a+b));
+        System.out.println("result2 = " + result2);
+
+        //Max and Min
+        Optional<Integer> min = numbers.stream().reduce(Integer::min);
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+        System.out.println("result5 = " + min.get());
+        System.out.println("result3 = " + max.get());
+        System.out.println("result4 = " + sum.get());
+
+
+
+
+    }
+}
